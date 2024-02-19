5f240eac15f94508dd7c4f0a5f2cbde6ec80a2ca
Anurag Kanwar <aigkaraig@gmail.com>
2024-02-19 12:37:32 +0530

    asdsgdfhgbxvzsdfgfhjnbv

--- index.js
+++ index.js
@@ -11,15 +11,17 @@ async function main() {
     let output = null
     let urls = [];
 
-    for await (const path of xmlpaths) {
-        const xml = await fs.readFile(path, "utf-8")
-        const result = await xml2js(xml, { ignoreComment: true, compact: true })
+    await Promise.all(
+        xmlpaths.map(async (path) => {
+            const xml = await fs.readFile(path, "utf-8")
+            const result = await xml2js(xml, { ignoreComment: true, compact: true })
 
-        if (output === null) {
-            output = result
-        }
-        urls = urls.concat(result.urlset.url)
-    }
+            if (output === null) {
+                output = result
+            }
+            urls = urls.concat(result.urlset.url)
+        })
+    )
     output.urlset.url = urls
     const result = await js2xml(output, { spaces: 2, compact: true })
     await fs.writeFile("output.xml", result)
